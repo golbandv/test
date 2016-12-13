783380a6ac64dcb8999421a3dde587158c360920
golbandv <golbandv@gmail.com>
2016-12-13 10:20:13 +0500

    минуты

---
+++ 2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>
+<?php
+class Article {
+    public $title;
+    private $news;
+    public function getTitle()
+    {
+        return $this->title.'<br>';
+    }
+
+    public function setNews($news)
+    {
+        $this->news = $news;
+    }
+    public function getNews()
+    {
+        return $this->news;
+    }
+}
+$a = new Article;
+$a->title='watch ';
+//$a->news='15';
+echo $a->getTitle();
+$a->setNews(5+8);
+echo $a->getNews();
+?>
+</h1>
+</body>
+</html>
+
---
+++ 3.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: remem
+ * Date: 12.12.2016
+ * Time: 22:41
+ */
